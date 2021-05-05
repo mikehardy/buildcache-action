8f80b9a16e6a28af4e16e9a05dc2d2444638c8b2
Mike Hardy <github@mikehardy.net>
2021-05-04 23:53:03 -0500

    feat: upload buildcache.log in the action via parameter, defaults false
    artifact storage can cost projects money, so this is disabled by default
    but figuring out why your cache stats are 100% miss requires the log
    
    Fixes #7 and #9

--- src/save.ts
+++ src/save.ts
@@ -1,5 +1,7 @@
+import * as artifact from '@actions/artifact'
 import * as cache from '@actions/cache'
 import * as core from '@actions/core'
+import * as io from '@actions/io'
 
 import { getCacheKeys, printStats } from './lib'
 
@@ -21,8 +23,48 @@ async function save(): Promise<void> {
   }
 }
 
+async function uploadBuildLog(): Promise<void> {
+  const artifactClient = artifact.create()
+  const artifactName = 'buildcache_log'
+
+  const ghWorkSpace = process.env.GITHUB_WORKSPACE
+  if (!ghWorkSpace) {
+    core.setFailed('process.env.GITHUB_WORKSPACE not set')
+    return
+  }
+
+  const files = [`${ghWorkSpace}/.buildcache/buildcache.log`]
+  const rootDirectory = `${ghWorkSpace}/.buildcache/`
+  const options = {
+    continueOnError: false
+  }
+
+  const uploadFlag = core.getInput('upload_buildcache_log')
+  if (uploadFlag && uploadFlag === 'true') {
+    try {
+      const uploadResponse = await artifactClient.uploadArtifact(
+        artifactName,
+        files,
+        rootDirectory,
+        options
+      )
+      core.info(
+        `Uploaded buildcache.log file, upload size ${uploadResponse.size}`
+      )
+    } catch (e) {
+      core.warning(`Unable to upload buildlog: ${e}`)
+    }
+  }
+  try {
+    await io.rmRF('./.buildcache/buildcache.log')
+  } catch (e) {
+    core.warning(`Unable to delete buildcache.log ${e}`)
+  }
+}
+
 async function run(): Promise<void> {
   await printStats()
+  await uploadBuildLog()
   await save()
 }
 
